d207026d31e48509ddc1eb4aa931f7f1613ae893
20.NIKHIL.DEVANATHAN <20.NIKHIL.DEVANATHAN@KE-115-8KTG9N2.ksd.edu>
2018-09-28 08:23:30 -0700

    <9-23-2016|ND>: Small commit that adds basic UI functionality.

--- tst/TestCAC/TestCAC.java
+++ tst/TestCAC/TestCAC.java
@@ -20,14 +20,11 @@ import javafx.stage.Stage;
  */
 public class TestCAC extends Application {
     
+    UIManager UiMan = new UIManager();
+    
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
-        
-        Scene scene = new Scene(root);
-        
-        stage.setScene(scene);
-        stage.show();
+        UiMan.startUI(stage);
     }
 
     /**
--- tst/TestCAC/UIManager.java
+++ tst/TestCAC/UIManager.java
@@ -5,10 +5,39 @@
  */
 package TestCAC;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.control.Button;
+
 /**
  *
  * @author nikhi
  */
-public class UIManager {
+public class UIManager implements Initializable {
     
+    @FXML
+    public void startUI(Stage stage) throws Exception {
+        
+        Parent root = FXMLLoader.load(getClass().getResource("StartScreen.fxml"));
+        
+        Scene startScreen = new Scene(root);
+        
+        stage.setScene(startScreen);
+        stage.show();
+    }
+    
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }
 }
